78a2fbb09a8da1057319f22032da91313194f817
JaiRaga <raga.j.santhosh@gmail.com>
2022-05-27 21:48:09 +0530

    :art: refactored code, added Flatlist to render a list of  horizontally scrollable categories

---
+++ screens/HomeScreen/styles.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    // backgroundColor: 'white'
+  },
+
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  image: {
+    width: 100,
+    height: 170,
+    resizeMode: 'cover',
+    borderRadius: 5,
+    margin: 5,
+  },
+});
+
+export default styles;
