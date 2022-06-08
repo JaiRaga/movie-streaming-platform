8f20eba4aa18afb39237b0ee5dbbd51dafdc3f22
JaiRaga <raga.j.santhosh@gmail.com>
2022-06-08 18:37:02 +0530

    :sparkles: changed video in video player on episode change

--- components/VideoPlayer/VideoPlayer.tsx
+++ components/VideoPlayer/VideoPlayer.tsx
@@ -1,8 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { View, Text } from 'react-native';
 import { Video } from 'expo-av';
+import { Playback } from 'expo-av/build/AV';
 import { Episode } from '../../types';
 import styles from './styles';
+// import { Playback } from 'expo-av/build/AV';
 
 interface VideoPlayerProps {
   episode: Episode;
@@ -10,8 +12,18 @@ interface VideoPlayerProps {
 
 const VideoPlayer = (props: VideoPlayerProps) => {
   const { episode } = props;
-  const video = useRef();
-  const [status, setStatus] = React.useState({});
+  const video = useRef<Playback>();
+  const [status, setStatus] = useState({});
+
+  useEffect(() => {
+    if (!video) {
+      return;
+    }
+    (async () => {
+      await video?.current?.unloadAsync();
+      await video?.current?.loadAsync({ uri: episode.video }, {}, false);
+    })();
+  }, [episode]);
 
   return (
     <View>
@@ -20,6 +32,9 @@ const VideoPlayer = (props: VideoPlayerProps) => {
         style={styles.video}
         source={{ uri: episode.video }}
         posterSource={{ uri: episode.poster }}
+        posterStyle={{
+          resizeMode: 'cover',
+        }}
         usePoster
         useNativeControls
         resizeMode="contain"
